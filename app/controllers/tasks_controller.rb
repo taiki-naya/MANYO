a08a80bcf4edae6eb162d725dc185e4038e87bee
taiki naya <taiki780528@gmail.com>
2021-08-17 06:59:11 +0000

    inplemented showing detail of a task action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,11 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
