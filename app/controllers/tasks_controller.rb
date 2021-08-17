2990b73d45a26dd94d7a55c0844e71c3cc9c94de
taiki naya <taiki780528@gmail.com>
2021-08-17 06:49:59 +0000

    inplemented editing task function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,17 +2,30 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-
+  
   def new
     @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: %(Your New Task is successfully created)
+      redirect_to tasks_path, notice: %(Your New Task was successfully created)
     else
       render :new
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: %(Your Task was successfully updated)
+    else
+      render :edit
     end
   end
 
