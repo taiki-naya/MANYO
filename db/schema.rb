02a65d48c607cac90755b1f4cf164212741125d7
taiki naya <taiki780528@gmail.com>
2021-08-17 02:16:19 +0000

    added a not null constraint in name column

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_020204) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
