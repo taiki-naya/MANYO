a97a235ca9320383a0e4dac93c0ea6fc3d4b68c9
taiki naya <taiki780528@gmail.com>
2021-08-17 07:26:29 +0000

    rename name column to title

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_020204) do
+ActiveRecord::Schema.define(version: 2021_08_17_072324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "title", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
