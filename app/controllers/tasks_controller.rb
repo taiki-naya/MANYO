273b1b32075e498cd03fb36ae58f81a97e9dc722
taiki naya <taiki780528@gmail.com>
2021-08-17 05:19:29 +0000

    added methods related create action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,23 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: %(Your New Task is successfully created)
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
+
 end
