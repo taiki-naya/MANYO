788d1ad32e708e97faf38c3aedc1b15eb28a01fd
taiki naya <taiki780528@gmail.com>
2021-08-20 07:47:51 +0000

    status column added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_024148) do
+ActiveRecord::Schema.define(version: 2021_08_20_074529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_08_20_024148) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "due_date", default: "1000-01-01", null: false
+    t.integer "status", default: 0
   end
 
 end
