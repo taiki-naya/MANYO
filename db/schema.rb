feec290885c6455741cf13ef91725f7d86e678bf
taiki naya <taiki780528@gmail.com>
2021-08-20 02:58:19 +0000

    added due_date column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_072324) do
+ActiveRecord::Schema.define(version: 2021_08_20_024148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_072324) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "due_date", default: "1000-01-01", null: false
   end
 
 end
