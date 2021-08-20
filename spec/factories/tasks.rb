b293af4b20ebdb0bfc59464b2d4469dfcd85a48e
taiki naya <taiki780528@gmail.com>
2021-08-20 15:20:20 +0000

    system spec updated

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :task do
   end
   factory :second_task, class: Task do
-    title { 'create' }
-    content { 'detail' }
+    title { '堪え難きを堪え' }
+    content { '忍び難きを忍び' }
+    due_date { '1945-8-15' }
+    status { 3 }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Tasks management system', type: :system do
     context 'case of creating a new task' do
       it 'created task will be displayed' do
         visit new_task_path
-        task = FactoryBot.create(:task, title: 'title', content: 'content')
+        task = FactoryBot.create(:second_task, title: 'title', content: 'content')
         visit tasks_path
         expect(page).to have_content 'content'
       end
@@ -13,11 +13,11 @@ RSpec.describe 'Tasks management system', type: :system do
     context 'when tasks are ordered by CREATED DATE as descending' do
       it ' the last created task will be displayed the top of tasks' do
         visit new_task_path
-        task = FactoryBot.create(:task, title: 'ordered', content: 'desc')
+        task = FactoryBot.create(:task, title: 'ordered', content: 'desc', status: 2)
         visit new_task_path
-        task = FactoryBot.create(:task, title: 'sort', content: 'reverse')
+        task = FactoryBot.create(:task, title: 'sort', content: 'reverse', status: 1)
         visit tasks_path
-        expect(all('td').first).to have_content 'sort'
+        expect(all('td').first).to have_content 'Waiting'
       end
     end
     context 'when tasks are ordered by DUE DATE as descending' do
@@ -29,6 +29,7 @@ RSpec.describe 'Tasks management system', type: :system do
         select '2021', from: 'task[due_date(1i)]'
         select '9', from: 'task[due_date(2i)]'
         select '1', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
         click_on '登録する'
         click_on 'Create a New Task', match: :first
         fill_in 'task[title]', with: 'immediately'
@@ -36,9 +37,10 @@ RSpec.describe 'Tasks management system', type: :system do
         select '2021', from: 'task[due_date(1i)]'
         select '8', from: 'task[due_date(2i)]'
         select '21', from: 'task[due_date(3i)]'
+        select 'Waiting', from: 'task[status]'
         click_on '登録する'
         click_on '[▼]'
-        expect(all('td').first).to have_content 'later'
+        expect(all('td').first).to have_content 'In Progress'
       end
     end
   end
@@ -59,6 +61,41 @@ RSpec.describe 'Tasks management system', type: :system do
         task = FactoryBot.create(:second_task, title: 'show page')
         visit task_path(task.id)
         expect(page).to have_content 'show page'
+      end
+    end
+  end
+
+  describe 'a function of searching for a task' do
+    context 'case of searching by TITLE' do
+      it 'a task containing the searched string will be displayed' do
+        task = FactoryBot.create(:second_task)
+        task = FactoryBot.create(:task, title: 'title', content: 'content', status: 1)
+        visit tasks_path
+        fill_in 'search', with: '堪え難きを堪え'
+        click_on 'Search'
+        expect(page).to have_content '忍び難きを忍び'
+      end
+    end
+    context 'case of searching by STATUS' do
+      it 'a task containing the searched status will be displayed' do
+        task = FactoryBot.create(:second_task)
+        task = FactoryBot.create(:task, title: 'Summer vacation', content: 'Go to the USA', status: 0)
+        visit tasks_path
+        select 'To Be Decided', from: 'task[status]'
+        click_on 'Search'
+        expect(page).to have_content 'USA'
+      end
+    end
+    context 'case of searching by TITLE and STATUS' do
+      it 'a task containing the searched title and status will be displayed' do
+        task = FactoryBot.create(:second_task)
+        task = FactoryBot.create(:second_task, status: 0)
+        task = FactoryBot.create(:task, title: 'Summer vacation', content: 'Go to the USA', status: 3)
+        visit tasks_path
+        fill_in 'search', with: '堪え難きを堪え'
+        select 'Completed', from: 'task[status]'
+        click_on 'Search'
+        expect(page).to have_content '忍び'
       end
     end
   end
