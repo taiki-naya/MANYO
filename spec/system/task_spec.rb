021046d30a0cbf45acf86b0d2f275da45def6456
taiki naya <taiki780528@gmail.com>
2021-08-22 13:42:52 +0000

    system spec updated

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,19 +4,37 @@ RSpec.describe 'Tasks management system', type: :system do
   describe 'a function of create a new task' do
     context 'case of creating a new task' do
       it 'created task will be displayed' do
-        visit new_task_path
-        task = FactoryBot.create(:second_task, title: 'title', content: 'content')
-        visit tasks_path
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'title'
+        fill_in 'task[content]', with: 'content'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '15', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
+        click_on '登録する'
         expect(page).to have_content 'content'
       end
     end
     context 'when tasks are ordered by CREATED DATE as descending' do
       it ' the last created task will be displayed the top of tasks' do
-        visit new_task_path
-        task = FactoryBot.create(:task, title: 'ordered', content: 'desc', status: 2)
-        visit new_task_path
-        task = FactoryBot.create(:task, title: 'sort', content: 'reverse', status: 1)
-        visit tasks_path
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'later'
+        fill_in 'task[content]', with: 'will do'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'immediately'
+        fill_in 'task[content]', with: 'doing'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '21', from: 'task[due_date(3i)]'
+        select 'Waiting', from: 'task[status]'
+        click_on '登録する'
         expect(all('td').first).to have_content 'Waiting'
       end
     end
@@ -48,9 +66,17 @@ RSpec.describe 'Tasks management system', type: :system do
   describe 'a function of displaying task index' do
     context 'case of transiting to index page' do
       it 'tasks index will be displayed' do
-        task = FactoryBot.create(:second_task, content: 'detail')
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'title'
+        fill_in 'task[content]', with: 'content'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
+        click_on '登録する'
         visit tasks_path
-        expect(page).to have_content 'detail'
+        expect(page).to have_content 'content'
       end
     end
   end
@@ -58,8 +84,16 @@ RSpec.describe 'Tasks management system', type: :system do
   describe 'a function of displaying a show page' do
     context 'case of transiting any tasks show page' do
       it 'detail of a applicable task will displayed' do
-        task = FactoryBot.create(:second_task, title: 'show page')
-        visit task_path(task.id)
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'transiting to'
+        fill_in 'task[content]', with: 'show page'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Show'
         expect(page).to have_content 'show page'
       end
     end
@@ -68,34 +102,83 @@ RSpec.describe 'Tasks management system', type: :system do
   describe 'a function of searching for a task' do
     context 'case of searching by TITLE' do
       it 'a task containing the searched string will be displayed' do
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:task, title: 'title', content: 'content', status: 1)
-        visit tasks_path
-        fill_in 'search', with: '堪え難きを堪え'
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'later'
+        fill_in 'task[content]', with: 'will do'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'In Progress', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'immediately'
+        fill_in 'task[content]', with: 'doing'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '21', from: 'task[due_date(3i)]'
+        select 'Waiting', from: 'task[status]'
+        click_on '登録する'
+        fill_in 'search', with: 'later'
         click_on 'Search'
-        expect(page).to have_content '忍び難きを忍び'
+        expect(page).to have_content 'will do'
       end
     end
     context 'case of searching by STATUS' do
       it 'a task containing the searched status will be displayed' do
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:task, title: 'Summer vacation', content: 'Go to the USA', status: 0)
-        visit tasks_path
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'Summer vacation'
+        fill_in 'task[content]', with: 'Go to the USA'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'Waiting', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'Winter vacation'
+        fill_in 'task[content]', with: 'Pending'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '21', from: 'task[due_date(3i)]'
         select 'To Be Decided', from: 'task[status]'
+        click_on '登録する'
+        select 'Waiting', from: 'task[status]'
         click_on 'Search'
         expect(page).to have_content 'USA'
       end
     end
     context 'case of searching by TITLE and STATUS' do
       it 'a task containing the searched title and status will be displayed' do
-        task = FactoryBot.create(:second_task)
-        task = FactoryBot.create(:second_task, status: 0)
-        task = FactoryBot.create(:task, title: 'Summer vacation', content: 'Go to the USA', status: 3)
-        visit tasks_path
-        fill_in 'search', with: '堪え難きを堪え'
-        select 'Completed', from: 'task[status]'
+        visit root_path
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'Summer vacation'
+        fill_in 'task[content]', with: 'Go to the USA'
+        select '2021', from: 'task[due_date(1i)]'
+        select '9', from: 'task[due_date(2i)]'
+        select '1', from: 'task[due_date(3i)]'
+        select 'Waiting', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'Winter vacation'
+        fill_in 'task[content]', with: 'Pending'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '21', from: 'task[due_date(3i)]'
+        select 'To Be Decided', from: 'task[status]'
+        click_on '登録する'
+        click_on 'Create a New Task', match: :first
+        fill_in 'task[title]', with: 'Spring vacation'
+        fill_in 'task[content]', with: 'Go sking'
+        select '2021', from: 'task[due_date(1i)]'
+        select '8', from: 'task[due_date(2i)]'
+        select '21', from: 'task[due_date(3i)]'
+        select 'To Be Decided', from: 'task[status]'
+        click_on '登録する'
+        fill_in 'search', with: 'vacation'
+        select 'To Be Decided', from: 'task[status]'
         click_on 'Search'
-        expect(page).to have_content '忍び'
+        expect(page).to have_content 'Spring'
       end
     end
   end
