9e33e7ef8c15b416a93eb1d1f7cb4298cef854a8
taiki naya <taiki780528@gmail.com>
2021-08-24 01:35:15 +0000

    User model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_134950) do
+ActiveRecord::Schema.define(version: 2021_08_24_013141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2021_08_22_134950) do
     t.integer "status", default: 0
     t.integer "priority"
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
