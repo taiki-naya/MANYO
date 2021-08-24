853dde87a0c7106e994d0a13e6946091135dac15
taiki naya <taiki780528@gmail.com>
2021-08-24 08:23:41 +0000

    set redirect to user page when user logged in and visit to new_user_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   skip_before_action :login_required, only: [:new, :create]
   before_action :set_user, only: [:show]
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to user_path(current_user.id)
+    else
+      @user = User.new
+    end
   end
   def create
     @user = User.new(user_params)
