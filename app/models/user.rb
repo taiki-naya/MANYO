07dca83e7758f8a763a2b8d407da4a08bf9f98cf
taiki naya <taiki780528@gmail.com>
2021-08-26 13:54:47 +0000

    set callback before admin update and destroy action

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   has_many :tasks, dependent: :destroy
+
+  before_update :check_admin_count_for_update
+  before_destroy :check_admin_count_for_destroy
+
+  private
+  def check_admin_count_for_update
+    users = User.all.select(:admin)
+    throw(:abort) if (users.where(admin: true).count == 1) && (self.admin == false) && (self.will_save_change_to_admin?)
+  end
+  def check_admin_count_for_destroy
+    users = User.all.select(:admin)
+    throw(:abort) if (users.where(admin: true).count == 1) && (self.admin == true)
+  end
 end
